72d7b8a5ac24075386d8abcba478b2c898ffaf82
RomanSlipchenko1 <romkazver@gmail.com>
2022-05-17 16:37:42 +0300

    Did C2 from chapter 2

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 import chapter2exercises.C0_CreateMatrix;
 import chapter2exercises.C0_ShowMatrix;
-import chapter2exercises.C1_SortUpCurrentLineOrColumn;
+import chapter2exercises.C2_MatrixShift;
 
 public class Main {
 
@@ -9,5 +9,8 @@ public class Main {
         int[][] matrix = C0_CreateMatrix.createMatrix();
         C0_ShowMatrix.showMatrix(matrix);
 
+        C2_MatrixShift.shiftMatrixDown(0, matrix);
+        System.out.println("_____________________");
+        C0_ShowMatrix.showMatrix(matrix);
     }
 }
---
+++ chapter2exercises/C2_MatrixShift.java
@@ -0,0 +1,65 @@
+package chapter2exercises;
+// Завдання С2 розділу 2
+// Виконати циклічний здвиг матриці на k позицій вверх, вниз, вліво, вправо
+
+
+public class C2_MatrixShift {
+
+    // Циклічний здвиг матриці вверх на задану кількість кроків
+    public static void shiftMatrixUp(int numOfSteps, int[][] matrix) {
+        int first;
+        for (int steps = 0; steps < numOfSteps; steps++) {
+            for (int column = 0; column < matrix.length; column++) {
+                first = matrix[0][column];
+                for (int element = 0; element < matrix[column].length - 1; element++) {
+                    matrix[element][column] = matrix[element + 1][column];
+                }
+                matrix[matrix.length - 1][column] = first;
+            }
+        }
+    }
+
+    // Циклічний здвиг матриці вниз на задану кількість кроків
+    public static void shiftMatrixDown(int numOfSteps, int[][] matrix) {
+        int first;
+        for (int steps = 0; steps < numOfSteps; steps++) {
+            for (int column = 0; column < matrix.length; column++) {
+                first = matrix[matrix.length - 1][column];
+                for (int element = matrix.length - 1; element > 0; element--) {
+                    matrix[element][column] = matrix[element -1][column];
+                }
+                matrix[0][column] = first;
+            }
+        }
+    }
+
+    // Циклічний здвиг матриці вліво на задану кількість кроків
+    public static void shiftMatrixLeft(int numOfSteps, int[][] matrix) {
+        int first;
+        for (int steps = 0; steps < numOfSteps; steps++) {
+            for (int line = 0; line < matrix.length; line++) {
+                first = matrix[line][0];
+                for (int element = 0; element < matrix[line].length - 1; element++) {
+                    matrix[line][element] = matrix[line][element + 1];
+                }
+                matrix[line][matrix[line].length - 1] = first;
+            }
+
+        }
+    }
+
+    // Циклічний здвиг матриці вправо на задану кількість кроків
+    public static void shiftMatrixRight(int numOfSteps, int[][] matrix) {
+        int first;
+        for (int steps = 0; steps < numOfSteps; steps++) {
+            for (int line = 0; line < matrix.length; line++) {
+                first = matrix[line][matrix[line].length - 1];
+                for (int element = matrix[line].length - 1; element > 0; element--) {
+                    matrix[line][element] = matrix[line][element - 1];
+                }
+                matrix[line][0] = first;
+            }
+        }
+    }
+
+}
