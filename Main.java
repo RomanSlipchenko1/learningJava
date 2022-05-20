50e3f3ab920e8e16f692af884634ee4084cd38f1
RomanSlipchenko1 <romkazver@gmail.com>
2022-05-20 10:49:46 +0300

    Did C3 from chapter 2

--- Main.java
+++ Main.java
@@ -1,16 +1,33 @@
-
 import chapter2exercises.C0_CreateMatrix;
 import chapter2exercises.C0_ShowMatrix;
-import chapter2exercises.C2_MatrixShift;
+import chapter2exercises.C3_ContinuousSequences;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[][] matrix = C0_CreateMatrix.createMatrix();
-        C0_ShowMatrix.showMatrix(matrix);
+        //int[][] matrix = C0_CreateMatrix.createMatrix();
+        //C0_ShowMatrix.showMatrix(matrix);
+        //System.out.println();
+        //System.out.println("_______________");
+        //C3_ContinuousSequences.continuousSequenceUp(matrix);
 
-        C2_MatrixShift.shiftMatrixDown(0, matrix);
-        System.out.println("_____________________");
-        C0_ShowMatrix.showMatrix(matrix);
+        int[][] matrix1 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int[][] matrix2 = {{16, 15, 14, 13}, {14, 6, 7, 0}, {0, 1, 6, 5}, {4, 3, 2, 1}};
+        int[][] matrix3 = {{16, 15, 14, 16}, {14, 6, 7, 0}, {0, 1, 6, 5}, {4, 3, 2, 1}};
+        int[][] matrix4 = {{16, 15, 14, 13}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+        int[][] matrix5 = {{100, 200, 300, 13}, {0, 0, 0, 0}, {0, 0, 0, 0}, {1, 2, 3, 4}};
+        int[][] matrix6 = {{16, 15, 14, 13}, {17, 0, 0, 0}, {0, 0, 0, 0}, {4, 3, 2, 1}};
+
+        int[][] matrix7 = {{16, 15, 14, 13}, {12, 11, 10, 9}, {8, 7, 6, 5}, {4, 3, 2, 1}};
+
+        int[][] matrix8 = {{-1, -2, -3, -4}, {-5, -6, -7, -8}, {-9, -10, -11, -12}, {-13, -14, -15, -16}};
+        C3_ContinuousSequences.continuousSequenceUp(matrix1);
+        C3_ContinuousSequences.continuousSequenceUp(matrix2);
+        C3_ContinuousSequences.continuousSequenceUp(matrix3);
+        C3_ContinuousSequences.continuousSequenceUp(matrix4);
+        C3_ContinuousSequences.continuousSequenceUp(matrix5);
+        C3_ContinuousSequences.continuousSequenceUp(matrix6);
+        C3_ContinuousSequences.continuousSequenceUp(matrix7);
+        C3_ContinuousSequences.continuousSequenceUp(matrix8);
     }
 }
--- chapter2exercises/C3_ContinuousSequences.java
+++ chapter2exercises/C3_ContinuousSequences.java
@@ -1,12 +1,28 @@
 package chapter2exercises;
-// Знайти в матриці і вивести найдовшу послідовність зростаючих елементів, які йдуть підряд
-// Знайти в матриці і вивести найдовшу послідовність спадаючих елементів, які йдуть підряд
+// Знайти в матриці і вивести найдовшу послідовність зростаючих елементів, які йдуть підряд (або першу)
+// Знайти в матриці і вивести найдовшу послідовність спадаючих елементів, які йдуть підряд (або першу)
+
+// Можна переробити так, щоб працювати з індексами, а не StringBuilder
+
+/*
+Можна переробити так, щоб працювати напряму з матрицею, без перетворення в лінійний масив,
+зробивши ітератор, який враховує перехід на новий рядок
+*/
+
+/* Цікаво було зробити методи саме з індикатором послідовності і двома SB, які "конкурують"
+між собою за звання більш підходящого значення.
+Індикатор послідовності дозволяє мати два режими роботи блока else-if:
+1 - коли поточний елемент і наступний не утворюють послідовність
+2 - коли поточний елемент і наступний не утворюють послідовність, але послідовність була до цього,
+отже, це кінець послідовності і потрібно виконати певні дії.
+Код громіздкий і алгоритм можна спростити, якщо працювати з індексами
+ */
 
 public class C3_ContinuousSequences {
 
-    // Вирівнює матрицю в лінійний масив
+    // Метод вирівнює квадратну матрицю в лінійний масив
     public static int[] roundMatrix(int[][] matrix) {
-        int[] result = new int[(matrix.length * matrix.length) + 1]; // +1 - проблема останнього індекса
+        int[] result = new int[(matrix.length * matrix.length)];
         int counter = 0;
         for (int line = 0; line < matrix.length; line++) {
             for (int element = 0; element < matrix[line].length; element++) {
@@ -18,55 +34,106 @@ public class C3_ContinuousSequences {
     }
 
     // Шукає і виводить найдовшу послідовність зростаючих елементів, які йдуть підряд
-    // В майбутньому переробити так, щоб працювати напряму з матрицею,
-    // а не перетворювати в лінійний масив
-    // Вирішити проблему останнього індекса, перевіряючи з попереднім індексом і стартуючи з 1
-
     public static void continuousSequenceUp(int[][] matrix) {
         int[] roundedMatrix = roundMatrix(matrix);
         StringBuilder result = new StringBuilder("");
+        int resultLength = 0;
         StringBuilder tempArray = new StringBuilder("");
+        int tempArrayLength = 0;
         boolean indicator = false;
 
-        // 0 1 2 3 0
         for (int index = 0; index < roundedMatrix.length - 1; index++) {
 
             if (roundedMatrix[index] < roundedMatrix[index + 1]) { // знайшли послідовність?
                 tempArray.append(roundedMatrix[index]).append(" ");
+                tempArrayLength++;
                 indicator = true;
-            }else if((roundedMatrix[index] >= roundedMatrix[index + 1]) && indicator) { // послідовність закінчилась?
+
+                // Додає значення останнього індексу в результат, якщо послідовність закінчується
+                // одночасно з масивом
+                if (index == roundedMatrix.length - 2) {
+                    tempArray.append(roundedMatrix[index + 1]).append(" ");
+                    tempArrayLength++;
+                }
+                // Цей else if спрацьовує лише після того, як послідовність почалась
+            } else if ((roundedMatrix[index] >= roundedMatrix[index + 1]) && indicator) { // послідовність закінчилась?
                 tempArray.append(roundedMatrix[index]);
+                tempArrayLength++;
                 indicator = false;
 
-                if (tempArray.length() > result.length()) {
+                if (tempArrayLength > resultLength) {
                     result = new StringBuilder(tempArray);
+                    resultLength = tempArrayLength;
                 }
                 // Очистити тимчасовий результат
                 tempArray = new StringBuilder("");
+                tempArrayLength = 0;
             }
 
         }
         // Якщо весь масив - неперервна послідовність
-        if (tempArray.length() > result.length()) {
+        if (tempArrayLength > resultLength) {
             result = new StringBuilder(tempArray);
+            resultLength = tempArrayLength;
         }
         // Вивести результат
-        if (result.length() > 0) {
+        if (resultLength > 1) {
+            System.out.println("Найдовша зростаюча послідовність: ");
             System.out.println(result.toString());
-        }else System.out.println("Зростаючих послідовностей не знайдено!");
+        } else System.out.println("Зростаючих послідовностей не знайдено!");
 
     }
 
+    // Шукає і виводить найдовшу послідовність спадаючих елементів, які йдуть підряд
+    public static void continuousSequenceDown(int[][] matrix) {
+        int[] roundedMatrix = roundMatrix(matrix);
+        StringBuilder result = new StringBuilder("");
+        int resultLength = 0;
+        StringBuilder tempArray = new StringBuilder("");
+        int tempArrayLength = 0;
+        boolean indicator = false;
 
+        for (int index = 0; index < roundedMatrix.length - 1; index++) {
 
+            if (roundedMatrix[index] > roundedMatrix[index + 1]) { // знайшли послідовність?
+                tempArray.append(roundedMatrix[index]).append(" ");
+                tempArrayLength++;
+                indicator = true;
 
+                // Додає значення останнього індексу в результат, якщо послідовність закінчується
+                // одночасно з масивом
+                if (index == roundedMatrix.length - 2) {
+                    tempArray.append(roundedMatrix[index + 1]).append(" ");
+                    tempArrayLength++;
+                }
+                // Цей else if спрацьовує лише після того, як послідовність почалась
+            } else if ((roundedMatrix[index] <= roundedMatrix[index + 1]) && indicator) { // послідовність закінчилась?
+                tempArray.append(roundedMatrix[index]).append(" ");
+                tempArrayLength++;
+                indicator = false;
 
+                if (tempArrayLength > resultLength) {
+                    result = new StringBuilder(tempArray);
+                    resultLength = tempArrayLength;
+                }
+                // Очистити тимчасовий результат
+                tempArray = new StringBuilder("");
+                tempArrayLength = 0;
+            }
 
+        }
+        // Якщо весь масив - неперервна послідовність
+        if (tempArrayLength > resultLength) {
+            result = new StringBuilder(tempArray);
+            resultLength = tempArrayLength;
+        }
+        // Вивести результат
+        if (resultLength > 1) {
+            System.out.println("Найбільша послідовність спадаючих чисел:");
+            System.out.println(result.toString());
+        } else System.out.println("Спадаючих послідовностей не знайдено!");
 
-
-
-
-
+    }
 
 
 }
